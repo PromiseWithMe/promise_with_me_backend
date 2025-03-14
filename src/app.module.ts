52e23c89cbb0eb6f29543e1a4c1d599af3488098
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-14 09:59:26 +0900

    ✨ (설정): ESLint, Prettier, NestJS 및 TypeScript 설정 파일 추가 📝 (문서): .gitignore 파일 추가하여 불필요한 파일 무시 💡 (구성): tsconfig 및 nest-cli 설정 파일 추가 🔧 (구성): package.json에 스크립트 및 의존성 추가 ♻️ (구성): AppModule 및 main.ts 파일 생성하여 기본 구조 설정

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+      })
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const port = process.env.HTTP_PORT;
+  await app.listen(port, () => {
+    Logger.log(`Server running on port ${port}`);
+  });
+}
+bootstrap();
