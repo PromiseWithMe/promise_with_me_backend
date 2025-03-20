0b1aca6c8439b435fe3a342a6d55d30c1be142ae
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-20 14:08:52 +0900

    refactor(promise): CreatePromiseDto 및 UpdatePromiseDto 삭제 및 리팩토링 feat(promise): CreatePromiseRequest DTO 추가 및 유효성 검사 추가

--- src/promise/dto/update-promise.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePromiseDto } from './create-promise.dto';
-
-export class UpdatePromiseDto extends PartialType(CreatePromiseDto) {}
