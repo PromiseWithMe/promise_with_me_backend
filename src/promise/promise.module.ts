08c89b011a0f022997d5260631524373425b16ea
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-20 14:09:16 +0900

    feat(promise): 약속 생성 기능 추가 및 사용자 이메일 처리 - 약속 생성 API 엔드포인트를 추가하여 사용자가 약속을 생성할 수 있도록 함 - 사용자 이메일을 기반으로 사용자 정보를 조회하고, 약속을 데이터베이스에 저장하는 로직을 구현함 - 트랜잭션을 사용하여 데이터 무결성을 보장함

--- src/promise/promise.module.ts
+++ src/promise/promise.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PromiseService } from './promise.service';
 import { PromiseController } from './promise.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entity/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Promise])],
+  imports: [TypeOrmModule.forFeature([Promise, User])],
   controllers: [PromiseController],
   providers: [PromiseService],
 })
--- src/promise/promise.service.ts
+++ src/promise/promise.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Promise } from './entity/promise.entity';
+import { DataSource, Repository } from 'typeorm';
+import { CreatePromiseRequest } from './dto/request/create-promise.request';
+import { User } from 'src/user/entity/user.entity';
+import { UserNotFoundException } from 'src/exception/custom-exception/user-not-found.exception';
+import { ServerException } from 'src/exception/custom-exception/server.exception';
 
 @Injectable()
-export class PromiseService {}
+export class PromiseService {
+  constructor(
+    @InjectRepository(Promise)
+    private readonly promiseRepository: Repository<Promise>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    private readonly datasource: DataSource,
+  ) {}
+
+  async createPromise(
+    userEmail: string,
+    createPromiseRequest: CreatePromiseRequest,
+  ) {
+    const { title, dayOfWeek } = createPromiseRequest;
+    const user = await this.userRepository.findOne({
+      where: { email: userEmail },
+    });
+    if (!user) throw new UserNotFoundException();
+
+    const qr = this.datasource.createQueryRunner();
+
+    await qr.connect();
+    await qr.startTransaction();
+
+    try {
+      await qr.manager.save(Promise, {
+        title,
+        dayOfWeek: dayOfWeek.toString(),
+        user,
+      });
+
+      await qr.commitTransaction();
+      return true;
+    } catch (error) {
+      await qr.rollbackTransaction();
+      console.log(error);
+
+      throw new ServerException();
+    } finally {
+      await qr.release();
+    }
+  }
+}
