3ed6ae573095a0144ca97787033aac2ddd268d75
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-23 20:34:26 +0900

    feat(ws-jwt-guard): WebSocket JWT 가드를 새로 추가 WebSocket 연결 시 JWT 인증을 처리하여 보안을 강화하기 위함.

---
+++ src/common/guard/ws-jwt-guard.ts
@@ -0,0 +1,66 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { InvalidTokenFormatException } from 'src/exception/ws-custom-exception/invalid-token-format.exception';
+import { EnvKeys } from '../enum/env-keys';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entity/user.entity';
+import { Repository } from 'typeorm';
+import { Reflector } from '@nestjs/core';
+import { Public } from '../decorator/public';
+import { ROLE } from '../enum/role';
+import { RBAC } from '../decorator/rbac';
+import { IsRefresh } from '../decorator/is-refresh';
+import { Socket } from 'socket.io';
+
+export class WsJwtGuard implements CanActivate {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const client: Socket = context.switchToWs().getClient();
+
+    if (this.reflector.get(Public, context.getHandler())) {
+      return true;
+    }
+
+    try {
+      const rawToken = client.handshake.headers.authorization;
+
+      if (!rawToken) throw new InvalidTokenFormatException();
+
+      const [bearer, token] = rawToken.split(' ');
+      if (bearer.toLowerCase() !== 'bearer')
+        throw new InvalidTokenFormatException();
+
+      const user = await this.userRepository.findOne({
+        where: { email: this.jwtService.decode(token).email },
+        select: ['email', 'role'],
+      });
+      if (!user) throw new InvalidTokenFormatException();
+
+      const requiredRole = this.reflector.get<ROLE>(RBAC, context.getHandler());
+      if (requiredRole !== undefined && user.role > requiredRole) {
+        throw new InvalidTokenFormatException();
+      }
+
+      const secretKey = this.reflector.get(IsRefresh, context.getHandler())
+        ? this.configService.get(EnvKeys.JWT_SECRET_REFRESH)
+        : this.configService.get(EnvKeys.JWT_SECRET);
+
+      client.data.user = await this.jwtService.verifyAsync(token, {
+        secret: secretKey,
+      });
+    } catch (err) {
+      throw new InvalidTokenFormatException();
+    }
+
+    return true;
+  }
+}
