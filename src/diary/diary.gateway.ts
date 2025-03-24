6f979f48437b5e3468c96f7e01c9a716837d060d
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-24 19:49:52 +0900

    feat(diary.gateway.ts): WebSocket 연결 및 인증 기능 추가 WebSocket 연결 시 JWT 인증을 적용하여 보안을 강화하고 일기 설정 기능을 추가함.

--- src/diary/diary.gateway.ts
+++ src/diary/diary.gateway.ts
@@ -1,7 +1,46 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { DiaryService } from './diary.service';
+import { ExecutionContext, UseGuards } from '@nestjs/common';
+import { GetUserEmail } from 'src/common/decorator/get-user';
+import { Socket } from 'socket.io';
+import { WsJwtGuard } from 'src/common/guard/ws-jwt-guard';
 
 @WebSocketGateway()
-export class DiaryGateway {
-  constructor(private readonly diaryService: DiaryService) {}
+@UseGuards(WsJwtGuard)
+@WebSocketGateway()
+export class DiaryGateway implements OnGatewayConnection {
+  constructor(
+    private readonly diaryService: DiaryService,
+    private readonly wsJwtGuard: WsJwtGuard,
+  ) {}
+
+  async handleConnection(client: Socket) {
+    try {
+      const createWsExecutionContext = () => {
+        return {
+          switchToWs: () => ({
+            getClient: () => client,
+          }),
+          getHandler: () => this.handleConnection,
+        } as unknown as ExecutionContext;
+      };
+
+      await this.wsJwtGuard.canActivate(createWsExecutionContext());
+      const userEmail = client.data.user.email;
+
+      this.diaryService.getDiary(userEmail, client);
+    } catch (error) {
+      client.disconnect();
+    }
+  }
+
+  @SubscribeMessage('setDiary')
+  setDiary(@GetUserEmail() userEmail: string, @MessageBody() data: string) {
+    this.diaryService.setDiary(userEmail, data);
+  }
 }
