53da6b6d911fc846418737e5052f3185b466c87c
ljyo2o9 <yggosxoljy@gmail.com>
2025-03-26 10:39:24 +0900

    feat(dto): 약속 요청 및 응답 DTO에서 요일 형식 변경 약속 요청에서 요일을 숫자 배열에서 문자열 배열로 변경하여 유효성 검사를 개선하고, 응답에서 요일 형식을 수정하여 일관성을 유지한다.

--- src/promise/dto/request/create-promise.request.ts
+++ src/promise/dto/request/create-promise.request.ts
@@ -1,5 +1,5 @@
-import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsNumber, IsString, Max, Min } from 'class-validator';
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
+import { dayOfWeeks } from 'src/common/set/day-of-weeks';
 
 export class CreatePromiseRequest {
   @IsNotEmpty({ message: '약속의 이름을 지어주세요' })
@@ -7,9 +7,6 @@ export class CreatePromiseRequest {
   title: string;
 
   @IsNotEmpty({ message: '날짜 형식을 지정해주세요' })
-  @IsNumber({}, { each: true, message: 'List는 숫자로만 이루어져야 합니다.' })
-  @Min(0, { each: true, message: '0~6 내외의 숫자만을 리스트에 넣어주세요' })
-  @Max(6, { each: true, message: '0~6 내외의 숫자만을 리스트에 넣어주세요' })
-  @Transform(({ value }) => value.sort())
-  dayOfWeek: number[];
+  @IsIn(dayOfWeeks, { each: true })
+  dayOfWeek: string[];
 }
